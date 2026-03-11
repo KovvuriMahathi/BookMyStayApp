952790dbb663fe5cf45d00bd65de552648a91bd9
Bhumika Dugar <bhumikadugar440@gmail.com>
2026-03-11 09:33:45 +0530

    KovvruiMahathi added UC-1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,6 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("welcome to the hotel booking management system");
+        System.out.println("system intitialized successfully;.");
+    }
+}
